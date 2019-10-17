0cd89c5c9c08aaed2ad2567635e1b4129fbbd0a2
Chen-Han Hsiao (Stanley) <chenhan.hsiao.tw@gmail.com>
2019-10-17 16:02:13 +0800

    Taipei ETL task

---
+++ dags/tpe_etl.py
@@ -0,0 +1,71 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.contrib.hooks.gcp_api_base_hook import GoogleCloudBaseHook
+from airflow.operators.dummy_operator import DummyOperator
+from operators.gcp_container_operator import GKEPodOperator
+
+default_args = {
+    "owner": "elin@mozilla.com",
+    "depends_on_past": False,
+    "start_date": datetime(2018, 11, 26),
+    "email": ["elin@mozilla.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=30),
+}
+
+
+def taipei_etl(
+    task_id,
+    gcp_conn_id="google_cloud_derived_datasets",
+    location="us-central1-a",
+    cluster_name="bq-load-gke-1",
+    name="tapei-etl",
+    namespace="default",
+    image="gcr.io/rocket-dev01/taipei-bi-etl",
+    image_pull_policy="Always",
+    arguments=[],
+    **kwargs,
+):
+    return GKEPodOperator(
+        task_id=task_id,
+        gcp_conn_id=gcp_conn_id,
+        project_id=GoogleCloudBaseHook(gcp_conn_id=gcp_conn_id).project_id,
+        location=location,
+        cluster_name=cluster_name,
+        name=name,
+        namespace=namespace,
+        image=image,
+        image_pull_policy=image_pull_policy,
+        arguments=[
+            "--debug",
+            "--date",
+            "{{ds}}",
+            "--next_execution_date",
+            "{{next_execution_date}}",
+        ]
+        + arguments,
+        **kwargs,
+    )
+
+
+with DAG("taipei_etl", default_args=default_args, schedule_interval=None) as dag:
+
+    gcp_conn_id = "google_cloud_derived_datasets"
+    connection = GoogleCloudBaseHook(gcp_conn_id=gcp_conn_id)
+
+    mango_events = taipei_etl(
+        "mango_events",
+        arguments=["--task", "bigquery", "--subtask", "mango_events"],
+        dag=dag,
+    )
+
+    mango_events_unnested = taipei_etl(
+        "mango_events_unnested",
+        arguments=["--task", "bigquery", "--subtask", "mango_events_unnested"],
+        dag=dag,
+    )
+
+    mango_events >> mango_events_unnested
